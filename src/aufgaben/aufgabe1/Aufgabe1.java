3a04a5f1de97b2f796e4647d99eb903bc1e1b584
Theissen, Daniel <Daniel.Theissen@malerkasse.de>
2022-11-16 14:41:42 +0100

    Fehler in Aufg1 korrigiert Der erste Ausdruck (abac) ist eine gültige Form von Var3

--- src/aufgaben/aufgabe1/Aufgabe1.java
+++ src/aufgaben/aufgabe1/Aufgabe1.java
@@ -2,7 +2,7 @@ package aufgaben.aufgabe1;
 
 public class Aufgabe1 {
     public static void main(String[] args) {
-        String regex = "(a(b|c)*a?)|(ab(b|c)+a)|(a(b|c)*b?(b|c)*)";
+        String regex = "(a(b|c)*a?)|(ab(b|c)+a)|(a(b|c)*ba?(b|c)*)";
         String[] toCheck = {"abac", "abaca", "aca", "abc", "abcab", "bc"};
         for(String s : toCheck){
             System.out.println(s + ":" + s.matches(regex));
